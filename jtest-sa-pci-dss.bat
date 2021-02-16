1f6d01cb826f0c3b30381f6054e66b483209c46d
whaaker <79113255+whaaker@users.noreply.github.com>
2021-02-15 19:34:28 -0800

    Update jtest-sa-pci-dss.bat

--- jtest-sa-pci-dss.bat
+++ jtest-sa-pci-dss.bat
@@ -10,7 +10,7 @@ echo Running PCI DSS 3.2 Test Configuration against build.id=%BUILD_ID%
 echo ===================================================================
 
 echo **1/1** Execute Static Analysis: PCI DSS 3.2
-call mvn jtest:jtest -Djtest.publish=true -Djtest.config="jtest.dtp://PCI DSS 3.2" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=%REPORT_DIR%\jtest-sa -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > %LOG_DIR%\jtest-pci-dss-%RUN_TIME%.log 2>&1
+call mvn jtest:jtest -Djtest.publish=true -Djtest.config="jtest.dtp://PCI DSS 3.2" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=%REPORT_DIR%\jtest-sa-pci_dss -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > %LOG_DIR%\jtest-pci-dss-%RUN_TIME%.log 2>&1
 
 echo =================================================================
 echo Finished PCI DSS 3.2 Test Configuration of build.id=%BUILD_ID%
