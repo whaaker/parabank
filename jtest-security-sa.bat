c65f2fa835f4a9f33d1519fd4e001964ab6960f8
demo <whaaker@parasoft.com>
2021-02-16 17:37:00 -0800

    updating for CWE Top 25

--- jtest-security-sa.bat
+++ jtest-security-sa.bat
@@ -13,7 +13,7 @@ echo Running Static Analysis and Unit Tests against build.id=%BUILD_ID%
 echo ===================================================================
 
 echo **1** Execute Static Analysis: Security Rules
-call mvn jtest:jtest -Djtest.config="Security Rules.properties" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=%REPORT_DIR%\jtest-sa-security -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > %LOG_DIR%\jtest-sa-security-%RUN_TIME%.log 2>&1
+call mvn jtest:jtest -Djtest.publish=true -Djtest.config="jtest.dtp://CWE Top 25 2019" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=%REPORT_DIR%\jtest-sa-security -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > %LOG_DIR%\jtest-sa-security-%RUN_TIME%.log 2>&1
 
 echo =================================================================
 echo Finished Static Analysis and Unit Testing of build.id=%BUILD_ID%
